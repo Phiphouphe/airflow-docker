48f9718aa361df547e931577f7312a1f4009e064
Ubuntu <ubuntu@ip-172-31-47-92.eu-west-3.compute.internal>
2026-03-04 01:45:50 +0000

    modif fichier init-db

--- init-db.sql
+++ init-db.sql
@@ -1,5 +1,3 @@
-cat > ~/airflow-docker/init-db.sql << 'EOF'
 CREATE USER mlflow WITH PASSWORD 'mlflow';
 CREATE DATABASE mlflow OWNER mlflow;
 GRANT ALL PRIVILEGES ON DATABASE mlflow TO mlflow;
-EOF
