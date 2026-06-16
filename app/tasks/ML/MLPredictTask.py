ecb20137fd772193ce59989e5fce6e774e798003
Ubuntu <ubuntu@ip-172-31-47-92.eu-west-3.compute.internal>
2026-06-16 17:53:52 +0000

    fix: prediction_date en TIMESTAMP WITH TIME ZONE UTC

--- app/tasks/ML/MLPredictTask.py
+++ app/tasks/ML/MLPredictTask.py
@@ -1,6 +1,5 @@
 import logging
 import time
-import pytz
 import pandas as pd
 
 import mlflow
@@ -148,10 +147,7 @@ class MLPredictTask(PythonOperator):
                 pred_db = df[["flight_number", "date", "dep_hour", "origin_airport", "destination_airport",
                             "departure_time_block", "day_of_week", "month", "is_cancelled"]].copy()
                 pred_db["is_delayed"] = predictions
-
-                paris = pytz.timezone("Europe/Paris")
-                pred_db["prediction_date"] = pd.Timestamp.now(tz=paris).replace(tzinfo=None)
-
+                pred_db["prediction_date"] = pd.Timestamp.now(tz="UTC")
                 pred_db["run_id"] = run_id
                 pred_db["model_name"] = model_name
                 pred_db["model_version"] = model_version
